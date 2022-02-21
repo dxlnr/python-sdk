33cf027c5137990f5aea120d8936f254af63d0c0
frommwonderland <daniel.illner@outlook.de>
2022-02-21 22:57:39 +0100

    adjusted accordingly

--- modalic/client/client.py
+++ modalic/client/client.py
@@ -1,4 +1,5 @@
 from modalic.client.grpc_client import Communicator
+from modalic.utils import common
 
 class Client(Communicator):
     r"""
@@ -6,9 +7,27 @@ class Client(Communicator):
     Serves as an simple layer and API that enables participating within
     Federated Learning process without designing the endpoints.
 
-    Args
-    ------------------------------------------------------------
-
+        Args:
+            cfg: Configuration setup defined by .toml
     """
-    def __init__(self):
+    def __init__(self,
+                 cfg):
+        super().__init__()
+
+    def set_weights(self, weights: common.Weights)):
+        r"""Set model weights from a list of NumPy ndarrays."""
+        state_dict = OrderedDict(
+            {
+                k: torch.tensor(v)
+                for k, v in zip(self.model.state_dict().keys(), weights)
+            }
+        )
+        self.model.load_state_dict(state_dict, strict=True)
+
+    def get_weights(self):
+        r"""Get model weights as a list of NumPy ndarrays."""
+        return [val.cpu().numpy() for _, val in self.model.state_dict().items()]
+
+    def run(self):
+        r"""Runs the whole process for a single modalic client."""
         pass
--- modalic/client/grpc_client.py
+++ modalic/client/grpc_client.py
@@ -1,6 +1,11 @@
 from abc import ABC, abstractmethod
 from typing import Optional
-from modalic.utils import protocol
+
+import grpc
+
+from modalic.utils import common
+from modalic.proto.mosaic_pb2_grpc import CommunicationStub
+from modalic.proto.mosaic_pb2 import ClientUpdate, ClientMessage
 
 class CommunicationLayer(ABC):
     r"""Abstract communicatio base layer for ensuring the grpc protocol.
@@ -23,19 +28,20 @@ class Communicator(CommunicationLayer):
     r"""Communicator class object implements the grpc protocol functionality
     which will be inherited by some client class object.
 
-    Args
-    ------------------------------------------------------------
+        Args:
+            server_address: static ip address of the aggregation server.
     """
-    def __init__(self):
-        pass
+    def __init__(self,
+                 server_address: str):
+        self.server_address = server_address
 
     @abstractmethod
-    def set_weights(self, weights: protocol.Weights):
+    def set_weights(self, weights: common.Weights):
         r"""Set model weights from a list of NumPy ndarrays."""
         raise NotImplementedError()
 
     @abstractmethod
-    def get_weights(self) -> protocol.Weights:
+    def get_weights(self) -> common.Weights:
         r"""Get model weights as a list of NumPy ndarrays."""
         raise NotImplementedError()
 
@@ -63,3 +69,36 @@ class Communicator(CommunicationLayer):
             channel = grpc.insecure_channel(server_address, options=channel_options)
             # log(INFO, "Opened insecure gRPC connection.")
         return (channel, CommunicationStub(channel))
+
+    def update(self,
+               dtype: str,
+               round_id: int,
+               stake: int,
+               loss: float):
+        r"""Sends an updated model version to the server.
+            Args:
+                dtype: Data Type of the trained model. Important as it determines the de-/serialization.
+                round_id: Training round id.
+                stake: Sets the number of samples the local model was trained on.
+                loss: Loss of the local model during training.
+        """
+        weights = self.get_weights()
+        parameters = parameters_to_proto(weights_to_parameters(weights, dtype=dtype, model_version=round_id))
+        process_meta = process_meta_to_proto(to_meta(round_id, loss))
+
+        (channel, stub) = self.grpc_connection(self.server_address)
+        send = stub.Update(ClientUpdate(id=self.cid, parameters=parameters, stake=stake, process_meta=process_meta))
+        channel.close()
+
+    def get_global_model(self):
+        r"""Client request to get the latest version of the global model from server."""
+        (channel, stub) = self.grpc_connection(self.server_address)
+        response = stub.GetGlobalModel(ClientMessage(id=self.cid))
+
+        params = parameters_from_proto(response)
+        if not params.tensor:
+            channel.close()
+        else:
+            weights = parameters_to_weights(params, self.model_shape)
+            self.set_weights(weights)
+            channel.close()
