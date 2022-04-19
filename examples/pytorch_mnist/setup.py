76f6ba763e0f63ca443ca51e7f42ffa466ea0aac
frommwonderland <daniel.illner@outlook.de>
2022-04-19 21:28:15 +0200

    small version update

--- examples/pytorch_mnist/setup.py
+++ examples/pytorch_mnist/setup.py
@@ -9,7 +9,7 @@ requirements = [
 
 
 def setup_package():
-    __version__ = '0.1'
+    __version__ = '0.1.0'
 
     setup(name='pytorch_mnist',
           description='Pytorch Federated Learning example classifying Hand-written.',
