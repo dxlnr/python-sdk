13d12a7417d78802660fdf22d23e3c0a0a71cf05
frommwonderland <daniel.illner@outlook.de>
2022-04-19 19:11:48 +0200

    extensions for example case & bug fix proto folder

--- examples/pytorch_mnist/setup.py
+++ examples/pytorch_mnist/setup.py
@@ -9,7 +9,7 @@ requirements = [
 
 
 def setup_package():
-    __version__ = '0.1.0'
+    __version__ = '0.1'
 
     setup(name='pytorch_mnist',
           description='Pytorch Federated Learning example classifying Hand-written.',
